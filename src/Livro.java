0873169429254a203b726a026742d3ef625eef6b
JessB <kacarioca2010@hotmail.com>
2025-11-06 22:28:43 -0300

    Adiciona construtor para inicializar Livro

--- src/Livro.java
+++ src/Livro.java
@@ -4,5 +4,8 @@ public class  Livro {
     private String titulo;
     private String autor;
 
-
+    public Livro(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+    }
 }
